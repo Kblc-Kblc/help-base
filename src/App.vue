947fbc298b887297066736dd34b7d4c1f472511e
Roman <kapkaevroman124@gmail.com>
2022-11-17 20:16:26 +0300

    ++++++

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<router-view v-slot="{ Component }">
+  <transition name="profile" class="animate__animated" enter-active-class="animated animate__fadeIn" mode="out-in">
+    <component :is="Component" />
+  </transition>
+</router-view>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="scss">
+@import "@/assets/css/style.scss";
+</style>
